b0da57aa4b94c77c3882b31856a37587a6e91231
Aliazhari <Aliazhari@hotmail.com>
2020-01-26 19:05:01 -0500

    src_01.c Hello world

---
+++ src_01.c
@@ -0,0 +1,15 @@
+/* ******************************************
+#  Author : Ali Azhari 
+#  Created On : Sun Jan 26 2020
+#  File : src_01.c
+# ****************************************** */
+
+#include <stdio.h>
+
+
+int main() {
+
+// Print hello world
+
+    printf("Hello world!\n");
+}
